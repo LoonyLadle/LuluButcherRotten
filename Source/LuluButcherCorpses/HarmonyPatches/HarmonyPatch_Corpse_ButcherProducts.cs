d81fbf2f893f5e83045096192986bbdc757c7c62
LoonyLadle <LoonyLadle@users.noreply.github.com>
2019-11-28 05:03:38 -0500

    Transpile to remove blood spawn

--- Source/LuluButcherCorpses/HarmonyPatches/HarmonyPatch_Corpse_ButcherProducts.cs
+++ Source/LuluButcherCorpses/HarmonyPatches/HarmonyPatch_Corpse_ButcherProducts.cs
@@ -1,6 +1,7 @@
 ﻿using Harmony;
 using RimWorld;
 using System.Collections.Generic;
+using System.Reflection.Emit;
 using Verse;
 
 #pragma warning disable IDE1006 // Naming Styles
@@ -10,6 +11,54 @@ namespace LoonyLadle.ButcherCorpses
 	[HarmonyPatch(typeof(Corpse), nameof(Corpse.ButcherProducts))]
 	public static class HarmonyPatch_Corpse_ButcherProducts
 	{
+		// Transpile to remove the butchery blood filth (because we want manual control over it).
+		public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			List<CodeInstruction> blockInstructions = new List<CodeInstruction>();
+			bool discard   = false;
+			bool keepGoing = false;
+
+			foreach (CodeInstruction instruction in instructions)
+			{
+				if ((instruction.opcode == OpCodes.Callvirt) && (instruction.operand == typeof(RaceProperties).GetProperty(nameof(RaceProperties.BloodDef)).GetGetMethod()))
+				{
+					discard   = true;
+					keepGoing = true;
+				}
+				else if ((instruction.opcode == OpCodes.Call) && (instruction.operand == typeof(FilthMaker).GetMethod(nameof(FilthMaker.MakeFilth))))
+				{
+					keepGoing = false;
+				}
+				else if ((instruction.opcode == OpCodes.Ldarg_0) && !keepGoing)
+				{
+					foreach (CodeInstruction blockInstruction in blockInstructions)
+					{
+						// "Discard" by yielding as Nop to preserve jump labels.
+						if (discard)
+						{
+							blockInstruction.opcode = OpCodes.Nop;
+							blockInstruction.operand = null;
+						}
+						yield return blockInstruction;
+					}
+					blockInstructions.Clear();
+					discard = false;
+				}
+				// Add the instruction to the block AFTER we've yielded the block,
+				// because our Ldarg_0 breakpoint is not meant to be a part of it.
+				blockInstructions.Add(instruction);
+			}
+			
+			// Yield any remaining instructions in the block.
+			foreach (CodeInstruction blockInstruction in blockInstructions)
+			{
+				yield return blockInstruction;
+			}
+
+			// We're done.
+			yield break;
+		}
+
 		public static IEnumerable<Thing> Postfix(IEnumerable<Thing> entries, Corpse __instance, Pawn butcher)
 		{
 			RotStage stage = __instance.GetRotStage();
@@ -45,6 +94,11 @@ namespace LoonyLadle.ButcherCorpses
 			{
 				butcher.needs.mood.thoughts.memories.TryGainMemory(MyDefOf.LuluButcherCorpses_ButcheredRotten);
 				FilthMaker.MakeFilth(butcher.Position, butcher.Map, ThingDefOf.Filth_CorpseBile, __instance.InnerPawn.LabelIndefinite());
+			}
+
+			if ((stage != RotStage.Dessicated) && (__instance.InnerPawn.RaceProps.BloodDef != null))
+			{
+				FilthMaker.MakeFilth(butcher.Position, butcher.Map, __instance.InnerPawn.RaceProps.BloodDef);
 			}
 		}
 	}
